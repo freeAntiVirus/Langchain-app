2153d387ff6e08bafd18a0fbbb0efc0eb8fb93b6
Yusra <z5417725@ad.unsw.edu.au>
2026-04-23 16:47:47 +1000

    localhost

--- hsc-ui/src/index.js
+++ hsc-ui/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'katex/dist/katex.min.css';
-// export const API_URL = "https://langchain-app-2.onrender.com"
-export const API_URL = "http://localhost:8000"
+export const API_URL = "https://langchain-app-2.onrender.com"
+// export const API_URL = "http://localhost:8000"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
